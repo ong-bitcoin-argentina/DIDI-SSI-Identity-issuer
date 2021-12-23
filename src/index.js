e5445fe09f5d43185a6c34277d52af8970595f47
Matias Oveja Smith <matias.ovejasmith@bitcoinargentina.org>
2021-12-23 10:08:38 -0300

    feat: Add API cache

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
+const apicache = require('apicache');
+const redis = require('redis');
 const express = require('express');
 
-const app = express();
 const Constants = require('../constants/Constants');
 const Messages = require('../constants/Messages');
 
-app.get('/identity/:identityId', (req, res) => {
+const app = express();
+
+app.use(
+  apicache
+    .options({
+      redisClient: redis.createClient(),
+      debug: false,
+      trackPerformance: true,
+      respectCacheControl: false,
+    })
+    .middleware(),
+);
+
+app.get('/cache', (_, res) => {
+  try {
+    res.json({
+      performance: apicache.getPerformance(),
+      index: apicache.getIndex(),
+    });
+  } catch (e) {
+    res.status(500);
+  }
+});
+
+app.get('/:identityId', (req, res) => {
   res.json({
     id: req.params.identityId,
   });
@@ -13,3 +38,7 @@ app.get('/identity/:identityId', (req, res) => {
 app.listen(Constants.PORT, () =>
   console.log(Messages.INDEX.MSG.RUNNING_ON + Constants.PORT),
 );
+
+module.exports = {
+  app,
+};
