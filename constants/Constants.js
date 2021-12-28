3c344e6eb421d3bf4cd25d407a6b9a7d1c7dbecc
DoloresGarciaE <doloresgarciae@hotmail.com>
2021-12-28 19:03:15 -0300

    feat: integration with api docs

--- constants/Constants.js
+++ constants/Constants.js
@@ -1,9 +1,18 @@
 const assert = require('assert');
 
 const PORT = 8089;
+const ENVIRONMENT = 'dev';
+const NAME = 'VUS';
+const VERSION = '1';
 
 assert.ok(PORT, 'No esta definida la varibale PORT');
+assert.ok(ENVIRONMENT, 'No esta definida la varibale ENVIRONMENT');
+assert.ok(NAME, 'No esta definida la varibale NAME');
+assert.ok(VERSION, 'No esta definida la varibale VERSION');
 
 module.exports = {
   PORT,
+  ENVIRONMENT,
+  NAME,
+  VERSION,
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 const apicache = require('apicache');
 const redis = require('redis');
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const Constants = require('../constants/Constants');
 const Messages = require('../constants/Messages');
 
 const app = express();
+
+const { NAME, VERSION, ENVIRONMENT } = require('../constants/Constants');
+
+/**
+ * Config de Swagger
+ */
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: NAME,
+      description: `Corriendo en el ambiente: ${ENVIRONMENT}. Para más información, visite la [documentación](https://docs.didi.org.ar/).`,
+      version: VERSION,
+    },
+    servers: [
+      {
+        url: '/',
+      },
+    ],
+  },
+  apis: ['./*.js', './routes/*.js'], // files containing annotations as above
+};
+const apiSpecification = swaggerJsdoc(options);
+/**
+ * @openapi
+ * /api-docs:
+ *   get:
+ *     description: Welcome to the jungle!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious webpage.
+ */
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiSpecification));
 
 app.use(
   apicache
