fa40f206664610201369668093a25a3e2e97edb4
DoloresGarciaE <dgarciaelissondo@gmail.com>
2022-01-19 16:25:18 -0300

    fix user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,36 +1,11 @@
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
-// const Hashing = require('./utils/Hashing');
-// const Encrypt = require('./utils/Encryption');
-// const EncryptedData = require('./dataTypes/EncryptedData');
-// const HashedData = require('./dataTypes/HashedData');
-// const { getImageUrl } = require('../utils/Helpers');
 
 // Registra la informacion correspondiente a un usuario de didi
-const UserSchema = new mongoose.Schema(
-  {
-    did: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-  },
-  {
-    toObject: {
-      virtuals: true,
-    },
-    toJSON: {
-      virtuals: true,
-    },
-  },
-);
-
-UserSchema.index(
-  { did: 1, deleted: 1 },
-  {
+// eslint-disable-next-line no-unused-vars
+const UserSchema = new mongoose.Schema({
+  did: {
+    type: String,
+    required: true,
     unique: true,
   },
-);
+});
