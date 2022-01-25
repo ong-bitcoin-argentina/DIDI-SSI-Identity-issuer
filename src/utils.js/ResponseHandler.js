adc24fa5c62c0b8552e7f9cc0609cd68d926af15
fedelmar <fsuarez91@gmail.com>
2022-01-25 09:50:53 -0300

    add ResponseHandler

---
+++ src/utils.js/ResponseHandler.js
@@ -0,0 +1,53 @@
+const Constants = require('../constants/Constants');
+
+/**
+ *  Mandar respuesta html genérica
+ */
+module.exports.sendHtml = function sendHtml(res, data) {
+  res.type('text/html; charset=UTF-8');
+  res.write(data);
+  return res.end();
+};
+
+/**
+ *  Mandar respuesta exitosa genérica
+ */
+module.exports.sendRes = function sendRes(res, data) {
+  res.type('application/json; charset=UTF-8');
+  return res.json({
+    status: 'success',
+    data,
+  });
+};
+
+/**
+ *  Mandar respuesta de error genérica
+ */
+module.exports.sendErr = function sendErr(res, err) {
+  // eslint-disable-next-line no-console
+  if (Constants.DEBUGG && err) console.log(err);
+
+  res.type('application/json; charset=UTF-8');
+  return res.json({
+    status: 'error',
+    errorCode: err && err.code,
+    message: err && err.message,
+  });
+};
+
+/**
+ *  Mandar respuesta de error con status custom
+ */
+module.exports.sendErrWithStatus = function sendErrWithStatus(
+  res,
+  err,
+  status = 500,
+) {
+  // eslint-disable-next-line no-console
+  if (Constants.DEBUGG && err) console.log(err);
+  return res.status(status).json({
+    status: 'error',
+    errorCode: err.code,
+    message: err.message,
+  });
+};
