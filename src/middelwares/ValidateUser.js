5393c06cd730093d843d0fe57ec97763a1d37ce6
fedelmar <fsuarez91@gmail.com>
2022-01-28 16:00:03 -0300

    create validation middelware

---
+++ src/middelwares/ValidateUser.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+const { USER } = require('../constants/Messages');
+const { DIDI_SERVER } = require('../constants/Constants');
+const { sendErrWithStatus } = require('../utils/ResponseHandler');
+
+const url = `${DIDI_SERVER}/user/verifyToken`;
+
+const validateUser = async (req, res, next) => {
+  try {
+    const jwt = req.header('Authorization');
+
+    const user = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(jwt),
+    });
+
+    if (!user) throw USER.ERR.VALIDATE;
+    next();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+    sendErrWithStatus(res, e, 401);
+  }
+};
+
+module.exports = {
+  validateUser,
+};
