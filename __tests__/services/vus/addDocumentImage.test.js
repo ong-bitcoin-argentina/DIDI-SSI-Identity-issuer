77e8b620ca31c03d4608b43a1e7d6666a4a40afa
DoloresGarciaE <dgarciaelissondo@gmail.com>
2022-01-31 14:38:05 -0300

    modified success tests

--- __tests__/services/vus/addDocumentImage.test.js
+++ __tests__/services/vus/addDocumentImage.test.js
@@ -1,22 +1,22 @@
-const fetchMock = require('fetch-mock');
+jest.mock('node-fetch');
+
+const fetch = require('node-fetch');
 const { addDocumentImage } = require('../../../src/services/vusService');
 const {
   missingOperationId,
   missingUserName,
   missingFile,
 } = require('../../../src/constants/serviceErrors');
-const { VUS_URLS } = require('../../../src/constants/Constants');
+
+const { successRespDocumentImage } = require('../mock/constants');
 
 describe('services/vus/addDocumentImage.test.js', () => {
   it('expect addDocumentImage OK', async () => {
-    expect.assertions(0);
-    fetchMock.post(VUS_URLS.addDocumentImage, {
-      imageDocument: {
-        code: 937,
-        message: 'Add document image success',
-      },
-    });
-    fetchMock.reset();
+    expect.assertions(2);
+    fetch.mockReturnValue(Promise.resolve(successRespDocumentImage));
+    const response = await addDocumentImage('operationId', 'userName', 'file');
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(response).toBe(successRespDocumentImage.json());
   });
   it('expect addDocumentImage to throw missing OperationId', async () => {
     expect.assertions(1);
