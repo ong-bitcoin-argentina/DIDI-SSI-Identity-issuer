3772849972ed681af47db3064069f2f85a5d4df1
DoloresGarciaE <dgarciaelissondo@gmail.com>
2022-01-31 15:55:33 -0300

    feat: endpoint addBack && deleted unused dependencies

--- src/controllers/vus/index.js
+++ src/controllers/vus/index.js
@@ -2,10 +2,12 @@ const { createVerification } = require('./createVerification');
 const { cancelVerification } = require('./cancelVerification');
 const { frontImage } = require('./addFront');
 const { addDocumentImage } = require('./addDocumentImage');
+const { backImage } = require('./addBack');
 
 module.exports = {
   createVerification,
   cancelVerification,
   frontImage,
   addDocumentImage,
+  backImage,
 };
