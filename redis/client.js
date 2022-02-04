da9436908e71bd6160d522e3231d29d97c685add
DoloresGarciaE <dgarciaelissondo@gmail.com>
2022-02-04 16:04:07 -0300

    fix problems

--- redis/client.js
+++ redis/client.js
@@ -12,21 +12,3 @@ const redisClient = redis.createClient({
 redisClient.on('error', (err) => {
   console.log(`Error ${err}`);
 });
-
-// Establece la clave "octocat" para un valor de "Mona the octocat"
-redisClient.set('octocat', 'Mona the Octocat', redis.print);
-// Establece una clave "octocat", campo para "species" y "value" para "Cat and Octopus"
-redisClient.hset('species', 'octocat', 'Cat and Octopus', redis.print);
-// Establece una clave para "octocat", campo para "species" y "value" para "Dinosaur and Octopus"
-redisClient.hset('species', 'dinotocat', 'Dinosaur and Octopus', redis.print);
-// Establece una clave para "octocat", campo para "species" y "value" para "Cat and Robot"
-redisClient.hset(['species', 'robotocat', 'Cat and Robot'], redis.print);
-// Obtiene todos los campos en la clave "species"
-
-redisClient.hkeys('species', (err, replies) => {
-  console.log(`${replies.length} replies:`);
-  replies.forEach((reply, i) => {
-    console.log(`    ${i}: ${reply}`);
-  });
-  redisClient.quit();
-});
