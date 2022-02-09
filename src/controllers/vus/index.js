f14cf3bdcf49b3db28cc054400d61fe656763401
DoloresGarciaE <dgarciaelissondo@gmail.com>
2022-02-09 12:42:37 -0300

    deleted controller addDocumentImage

--- src/controllers/vus/index.js
+++ src/controllers/vus/index.js
@@ -1,11 +1,9 @@
 const { createVerification } = require('./createVerification');
 const { cancelVerification } = require('./cancelVerification');
-const { addDocumentPhoto } = require('./addDocumentPhoto');
 const { addDocumentImage } = require('./addDocumentImage');
 
 module.exports = {
   createVerification,
   cancelVerification,
-  addDocumentPhoto,
   addDocumentImage,
 };
