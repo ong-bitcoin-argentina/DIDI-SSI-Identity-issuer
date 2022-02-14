ea96238a1699961c4bb9345af1df7f47313e05a2
fedelmar <fsuarez91@gmail.com>
2022-02-14 20:19:39 -0300

    fix redis Service

--- src/services/RedisService.js
+++ src/services/RedisService.js
@@ -1,50 +1,37 @@
+/* eslint-disable no-console */
 const redis = require('redis');
-const { REDIS_URI } = require('../constants/Constants');
+const { REDIS_URI, PREFIJO_REDIS } = require('../constants/Constants');
 const { missingKey, missingValue } = require('../constants/serviceErrors');
 
-let client;
+const config = {
+  url: REDIS_URI,
+};
+const client = redis.createClient(config);
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
 (async () => {
-  client = await redis.createClient({ url: REDIS_URI });
-
-  // eslint-disable-next-line no-console
-  client.on('error', (err) => console.log('Redis Client Error', err));
-
   await client.connect();
 })();
 
 const get = async (key) => {
   if (!key) throw missingKey;
-  try {
-    const value = await client.get(key);
-    return value;
-  } catch (err) {
-    return err;
-  }
+  return client.get(`${PREFIJO_REDIS}-${key}`);
 };
 
 const set = async (key, value) => {
   if (!key) throw missingKey;
   if (!value) throw missingValue;
-  try {
-    const response = await client.setEx(key, 864000, value);
-    return response;
-  } catch (err) {
-    return err;
-  }
+  return client.setEx(`${PREFIJO_REDIS}-${key}`, 864000, value);
 };
 
 const del = async (key) => {
   if (!key) throw missingKey;
-  try {
-    const response = await client.del(key);
-    return response;
-  } catch (err) {
-    return err;
-  }
+  return client.del(`${PREFIJO_REDIS}-${key}`);
 };
 
 const disconnect = () => {
-  return client.disconnect();
+  return client.quit();
 };
 
 module.exports = {
@@ -52,4 +39,5 @@ module.exports = {
   set,
   del,
   disconnect,
+  client,
 };
