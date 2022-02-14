7da9f7d5c1e6960f56bf4c51bda4b3b7f0ad57da
fedelmar <fsuarez91@gmail.com>
2022-02-14 20:02:59 -0300

    update service

--- src/services/RedisService.js
+++ src/services/RedisService.js
@@ -3,25 +3,18 @@ const redis = require('redis');
 const { REDIS_URI, PREFIJO_REDIS } = require('../constants/Constants');
 const { missingKey, missingValue } = require('../constants/serviceErrors');
 
-let client;
+const config = {
+  url: REDIS_URI,
+};
+const client = redis.createClient(config);
+
+client.on('connect', () => console.log('Redis Client connect'));
+client.on('ready', () => console.log('Redis Client ready'));
+client.on('end', () => console.log('Redis Client end'));
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.on('reconnecting', () => console.log('Redis Client reconnecting'));
 (async () => {
-  try {
-    const config = {
-      url: REDIS_URI,
-      password: process.env.REDIS_PASSWORD,
-    };
-    client = await redis.createClient(config);
-
-    client.on('connect', () => console.log('Redis Client connect'));
-    client.on('ready', () => console.log('Redis Client ready'));
-    client.on('end', () => console.log('Redis Client end'));
-    client.on('error', (err) => console.log('Redis Client Error', err));
-    client.on('reconnecting', () => console.log('Redis Client reconnecting'));
-
-    await client.connect();
-  } catch (e) {
-    console.log(e);
-  }
+  await client.connect();
 })();
 
 const get = async (key) => {
