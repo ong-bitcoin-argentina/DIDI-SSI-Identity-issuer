66df8cf729f4f76c14b8cc62a8b23bcd2531e59d
fedelmar <fsuarez91@gmail.com>
2022-02-23 16:36:16 -0300

    move /cache & /api-docs to serviceRoutes

---
+++ src/routes/utils/swaggerConfig.js
@@ -0,0 +1,26 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const { NAME, VERSION, ENVIRONMENT } = require('../../constants/Constants');
+
+/**
+ * Config de Swagger
+ */
+const options = {
+  definition: {
+    openapi: '3.0.3',
+    info: {
+      title: NAME,
+      description: `Corriendo en el ambiente: ${ENVIRONMENT}. Para más información, visite la [documentación](https://docs.didi.org.ar/).`,
+      version: VERSION,
+    },
+    servers: [
+      {
+        url: '/api',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+const apiSpecification = swaggerJsdoc(options);
+
+module.exports = apiSpecification;
