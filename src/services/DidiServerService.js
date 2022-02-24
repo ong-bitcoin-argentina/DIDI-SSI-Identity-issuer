a516c0e78809dd235af2f0175b30d8397be9fbcf
fedelmar <fsuarez91@gmail.com>
2022-02-24 16:09:11 -0300

    add tests & update verifyToken service

--- src/services/DidiServerService.js
+++ src/services/DidiServerService.js
@@ -8,19 +8,23 @@ const { missingToken } = require('../constants/serviceErrors');
  */
 const verifyToken = async (token) => {
   if (!token) throw missingToken;
-  const response = await fetch(`${DIDI_SERVER}/user/verifyToken`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ jwt: token }),
-  });
-  const { status } = await response.json();
+  try {
+    const response = await fetch(`${DIDI_SERVER}/user/verifyToken`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ jwt: token }),
+    });
+    const { status } = await response.json();
 
-  if (status !== 'success') {
-    return false;
+    if (status !== 'success') {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    return error;
   }
-  return true;
 };
 
 module.exports = {
